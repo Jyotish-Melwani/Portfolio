f19b1b2f929fe02a71d06fb71226856a209110ce
Jyotish Melwani <jyotishmelwani@gmail.com>
2024-10-29 20:22:08 -0400

    package changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+import { NextConfig } from 'next';
+
+const withBundleAnalyzer = require('@next/bundle-analyzer');
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  webpack(config: any) {
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        chunks: 'all',
+        maxSize: 250000, // Ensure chunks are below 25 MiB
+      },
+    };
+    return config;
+  },
+};
 
 module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-});
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig);
